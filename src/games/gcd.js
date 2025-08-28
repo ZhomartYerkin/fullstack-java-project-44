0411854115453923cfdefe1fe1c3052a5b20616c
Zhomart Yerkin <zyerkin@gmail.com>
2025-08-29 00:36:54 +0500

    feat(gcd): add brain-gcd game

---
+++ src/games/gcd.js
@@ -0,0 +1,32 @@
+import runGame from '../index.js';
+
+const RULES = 'Find the greatest common divisor of given numbers.';
+
+const abs = (n) => (n < 0 ? -n : n);
+
+
+const gcd = (a, b) => {
+  let x = abs(a);
+  let y = abs(b);
+
+  while (y !== 0) {
+    const r = x % y;
+    x = y;
+    y = r;
+  }
+  return x; 
+};
+
+const rand = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const getRoundData = () => {
+  const a = rand(1, 100);
+  const b = rand(1, 100);
+  return {
+    question: `${a} ${b}`,
+    answer: gcd(a, b),
+  };
+};
+
+export default () => runGame(RULES, getRoundData);
+
